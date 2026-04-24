385e615ef85a41165388bd5d7e22b7eb89f8f8f6
Nurdan Karahan <nurdankarahan001@gmail.com>
2026-04-24 17:03:21 +0300

    [+] add program to print prime numbers between 1 and 100.

---
+++ Java101/loops/prime-number-printer/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1 ile 100 arasındaki asal sayılar:");
+
+        for (int number = 2; number <= 100; number++) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Java101/loops/prime-number-printer/README.md
@@ -0,0 +1,17 @@
+# Prime Number Printer
+
+Bu program, 1 ile 100 arasındaki asal sayıları ekrana yazdırır.
+
+## Asal Sayı Nedir?
+Asal sayı, yalnızca 1 ve kendisiyle tam bölünebilen sayıdır. Örneğin, 2, 3, 5, 7 asal sayılardır.
+
+## Kullanılan Konular
+- Java
+- for döngüsü
+- if koşulu
+- asal sayı kontrolü
+
+## Çalıştırma
+```bash
+javac Main.java
+java Main
